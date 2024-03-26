9570cac157483fb06b26a9497d9a2d34820221f2
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-26 22:18:40 +0300

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -2,12 +2,12 @@ import fg from 'api-dylux';
 
 const handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) {
-    throw `✳️ Please send the link of a Facebook video\n\n📌 EXAMPLE :\n*${usedPrefix + command}* https://www.facebook.com/Ankursajiyaan/videos/981948876160874/?mibextid=rS40aB7S9Ucbxw6v`;
+    throw `*🌸𝑀𝐴𝐾𝐼𝑀𝐴-𝐵𝛩𝑇🌸*\n\n*مثال*\n*${usedPrefix + command}* https://wa.me/967733707084*`;
   }
 
   const urlRegex = /^(?:https?:\/\/)?(?:www\.)?(?:facebook\.com|fb\.watch)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
   if (!urlRegex.test(args[0])) {
-    throw '⚠️ PLEASE GIVE A VALID URL.';
+    throw '*ارسل رابط فيسبوك صحيح*';
   }
 
   m.react(rwait);
@@ -15,9 +15,9 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
     const result = await fg.fbdl(args[0]);
     const tex = `
-⊱ ─── {* XLICON FBDL*} ─── ⊰
+*⊱ ─── {🌸𝑀𝐴𝐾𝐼𝑀𝐴-𝐵𝛩𝑇🌸} ─── ⊰*
 ↳ *VIDEO TITLE:* ${result.title}
-⊱ ────── {⋆♬⋆} ────── ⊰`;
+*⊱ ────── {⋆♬⋆} ────── ⊰*`;
 
     const response = await fetch(result.videoUrl);
     const arrayBuffer = await response.arrayBuffer();
@@ -27,13 +27,13 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
     m.react(done);
   } catch (error) {
     console.log(error);
-    m.reply('⚠️ An error occurred while processing the request. Please try again later.');
+    m.reply('*حاول مره اخرى او تحقق ممن الرابط اذا كان صحيح*');
   }
 };
 
 handler.help = ['facebook <url>'];
 handler.tags = ['downloader'];
-handler.command = /^((facebook|fb)(downloder|dl)?)$/i;
+handler.command = /^((facebook|فيسبوك|فيس|fb)(downloder|dl)?)$/i;
 handler.diamond = true;
 
 export default handler;
